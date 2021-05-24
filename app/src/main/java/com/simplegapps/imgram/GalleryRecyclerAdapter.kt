ecdafebfc4b5496ae5c099657de2d55bb4191964
Gustavo Ramirez <gustyadolf@gmail.com>
2021-05-24 21:23:29 +0200

    added something

--- app/src/main/java/com/simplegapps/imgram/GalleryRecyclerAdapter.kt
+++ app/src/main/java/com/simplegapps/imgram/GalleryRecyclerAdapter.kt
@@ -1,16 +1,15 @@
 package com.simplegapps.imgram
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.simplegapps.imgram.databinding.GalleryItemBinding
 
 class GalleryRecyclerAdapter : RecyclerView.Adapter<GalleryViewHolder>() {
 
-    private val imageList: List<Image> = emptyList()
+    var imageList: List<Image> = emptyList()
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GalleryViewHolder  =
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GalleryViewHolder =
         GalleryItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
             .run {
                 GalleryViewHolder(this)
@@ -19,13 +18,18 @@ class GalleryRecyclerAdapter : RecyclerView.Adapter<GalleryViewHolder>() {
 
     override fun onBindViewHolder(holder: GalleryViewHolder, position: Int) {
         val image: Image = imageList[position]
+        holder.bind(image)
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = imageList.size
+
 }
 
-data class GalleryViewHolder(val binding: GalleryItemBinding) : RecyclerView.ViewHolder(binding.root) {
+data class GalleryViewHolder(val binding: GalleryItemBinding) :
+    RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(image: Image) {
+        binding.textView.text = image.url
+    }
 
 }
--- app/src/main/java/com/simplegapps/imgram/MainActivity.kt
+++ app/src/main/java/com/simplegapps/imgram/MainActivity.kt
@@ -2,10 +2,21 @@ package com.simplegapps.imgram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.simplegapps.imgram.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        // Se puede usar run pero devuelve void en este caso
+        // Usamos also para ello
+        val binding = MainActivityBinding.inflate(layoutInflater).also {
+            setContentView(it.root)
+        }
+
+        val adapter = GalleryRecyclerAdapter()
+        binding.galleryRecyclerView.adapter = adapter
+
+        adapter.imageList = listOf(Image("url"))
+
     }
 }
